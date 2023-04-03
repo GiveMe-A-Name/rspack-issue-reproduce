f383361ce194da6c2b9a01687d2af292256cd037
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-04-03 16:33:53 +0800

    fix: format config

--- builtins-provide/rspack.config.js
+++ builtins-provide/rspack.config.js
@@ -1,31 +1,31 @@
-const nodeLibsBrowser = require('node-libs-browser');
-console.log('nodeLibsBrowser.process', nodeLibsBrowser.process);
+const nodeLibsBrowser = require("node-libs-browser");
+console.log("nodeLibsBrowser.process", nodeLibsBrowser.process);
 /**
  * @type {import('@rspack/cli').Configuration}
  */
 module.exports = {
-	context: __dirname,
-	entry: {
-		main: "./src/main.jsx"
-	},
-	stats: { preset: 'normal' },
-	builtins: {
-		html: [
-			{
-				template: "./index.html"
-			}
-		],
-		provide: {
-			aaa: ["./aaa"],
-            process: [nodeLibsBrowser.process],
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: "asset"
-			}
-		]
-	}
+  context: __dirname,
+  entry: {
+    main: "./src/main.jsx",
+  },
+  stats: { preset: "normal" },
+  builtins: {
+    html: [
+      {
+        template: "./index.html",
+      },
+    ],
+    provide: {
+      aaa: ["./aaa"],
+      process: [nodeLibsBrowser.process],
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+    ],
+  },
 };
