4d5ca28b27d195371f407f6e1428f8932d5bae41
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-04-03 16:33:13 +0800

    chore: add builtins-provide demo

--- builtins-provide/rspack.config.js
+++ builtins-provide/rspack.config.js
@@ -1,3 +1,5 @@
+const nodeLibsBrowser = require('node-libs-browser');
+console.log('nodeLibsBrowser.process', nodeLibsBrowser.process);
 /**
  * @type {import('@rspack/cli').Configuration}
  */
@@ -6,20 +8,17 @@ module.exports = {
 	entry: {
 		main: "./src/main.jsx"
 	},
+	stats: { preset: 'normal' },
 	builtins: {
 		html: [
 			{
 				template: "./index.html"
 			}
 		],
-		// presetEnv: {
-		// 	targets: [
-		// 	  '> 0.01%',
-		// 	  'not dead',
-		// 	  'not op_mini all'
-		// 	],
-		// 	coreJs: '3.26'
-		//   }
+		provide: {
+			aaa: ["./aaa"],
+            process: [nodeLibsBrowser.process],
+		}
 	},
 	module: {
 		rules: [
